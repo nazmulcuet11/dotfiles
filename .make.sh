9c20f6214621e04f20476e6824d6832dfc57fc83
nazmulcuet11 <nazmulcuet11@gmail.com>
2019-04-07 00:31:25 +0600

    update make.sh

--- .make.sh
+++ .make.sh
@@ -2,17 +2,19 @@
 # .make.sh
 # This script creates symlinks from ~/ to dotfiles dir
 
-dir=~/dotfiles
-olddir=~/dotfiles-old
-files=".bashrc .vimrc .zshrc .gitconfig"
+# dir=~/dotfiles
+# olddir=~/dotfiles-old
+# files=".bashrc .vimrc .zshrc .gitconfig"
 
-echo "Creating $olddir for backup of any existing dotfiles in ~ dir ..."
-mkdir --parents $olddir
-echo "... complete."
+# echo "Creating $olddir for backup of any existing dotfiles in ~ dir ..."
+# mkdir --parents $olddir
+# echo "... complete."
 
-for file in $files; do
-	echo "Moving $file from ~ to $olddir"
-	mv ~/$file $olddir/
-	echo "Creating symlink to $file in home directory"
-	ln --symbolic $dir/$file ~/$file
-done
+# for file in $files; do
+# 	echo "Moving $file from ~ to $olddir"
+# 	mv ~/$file $olddir/
+# 	echo "Creating symlink to $file in home directory"
+# 	ln --symbolic $dir/$file ~/$file
+# done
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
